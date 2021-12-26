9535e8ae46f41a35310325226ca093889a4f6b7c
Vstay <vstay@qq.com>
2021-12-26 23:43:02 +0800

    数据类型

---
+++ src/hello/demo/DataType.java
@@ -0,0 +1,25 @@
+package hello.demo;
+
+public class DataType {
+    public static void main(String[] args) {
+        // 八大基本数据类型
+
+        // 整数
+        int num1 = 10;  //最常用
+        byte num2 = 20;
+        short num3 = 30;
+        long num4 = 30L;    //Long类型要在数字后面加个L
+
+        // 小数:浮点数
+        float num5 = 50.1F; //默认是double,float类型要在后面加L
+        double num6 = 3.13290123;
+
+        // 字符
+        char name = '国';
+        // 字符串,string不是关键字,是类
+        // string namea = "中国";
+
+        // 布尔值
+        boolean flag = true;
+    }
+}
--- src/hello/demo/Hello.java
+++ src/hello/demo/Hello.java
@@ -1,7 +1,9 @@
 package hello.demo;
 
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("hello,world");
+
+
     }
 }
