0cfce9f4ab5cf4d9d1c5779ac616026bac2e4d21
Vstay <vstay@qq.com>
2021-12-27 01:18:47 +0800

    包机制

---
+++ src/hello/demo/Dome06_package.java
@@ -0,0 +1,11 @@
+package hello.demo;
+
+import java.util.Date;
+import hello.demo.Dome01_Hello;
+import hello.demo.*;
+
+public class Dome06_package {
+    public static void main(String[] args) {
+        Date a = new Date();
+    }
+}
