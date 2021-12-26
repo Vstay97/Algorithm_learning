3aaf29fc348b4f90af38629f677c774a0ac0cca9
Vstay <vstay@qq.com>
2021-12-27 01:33:30 +0800

    JavaDoc生成文档

--- src/hello/demo/Dome06_Package.java
+++ src/hello/demo/Dome06_Package.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import hello.demo.Dome01_Hello;
 import hello.demo.*;
 
-public class Dome06_package {
+public class Dome06_Package {
     public static void main(String[] args) {
         Date a = new Date();
     }
---
+++ src/hello/demo/Dome07_Doc.java
@@ -0,0 +1,22 @@
+package hello.demo;
+
+/**
+ * @author Vstay
+ * @version 1.8
+ * @since 1.8
+ */
+public class Dome07_Doc {
+    // 属性
+    String name;
+
+    // 方法
+    /**
+     * @author Vstay
+     * @param args
+     * @return
+     * @throws Exception
+     */
+    public String test(String args) throws Exception{
+        return name;
+    }
+}
