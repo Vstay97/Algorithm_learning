9a6e1624c0aa979d5ac639fa76b7f61ec93a7ef4
Vstay <vstay@qq.com>
2022-01-08 17:00:32 +0800

    nextInt方法,nextFloat方法...

---
+++ src/hello/demo/Dome10_Scanner_plus.java
@@ -0,0 +1,23 @@
+package hello.demo;
+
+import java.util.Scanner;
+
+public class Dome10_Scanner_plus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入");
+        int i =0;
+        float j = 0.1f;
+        if(scanner.hasNextInt()){
+            i = scanner.nextInt();
+            System.out.println("整数为: "+i);
+        }
+        else{
+            System.out.println("输入的不是整数");
+        }
+        if (scanner.hasNextFloat()) {
+            j = scanner.nextFloat();
+            System.out.println("小数为: "+j);
+        }
+    }
+}
