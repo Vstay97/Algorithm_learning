eb5bddb5f770ac21dbe5657ba27bf54f69bceda6
Vstay <vstay@qq.com>
2022-01-09 19:29:53 +0800

    方法的重载

---
+++ src/hello/demo/Dome12_Overload.java
@@ -0,0 +1,16 @@
+package hello.demo;
+
+public class Dome12_Overload {
+    public static void main(String[] args) {
+        double max = max(2.2,3.5);
+        System.out.println(max);
+    }
+    public static int max(int a,int b ){
+        System.out.println("int max:");
+        return a>b ? a:b;
+    }
+    public static double max(double a,double b){
+        System.out.println("double max");
+        return a>b ? a:b;
+    }
+}
