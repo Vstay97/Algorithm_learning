d2fb5592472cf36bfb721bfb6eabc5c1995e9673
Vstay <vstay@qq.com>
2022-01-09 19:29:40 +0800

    增强for循环

---
+++ src/hello/demo/Dome11_ForPlus.java
@@ -0,0 +1,12 @@
+package hello.demo;
+
+import java.util.Scanner;
+
+public class Dome11_ForPlus {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        for(int i : numbers){
+            System.out.println(i);
+        }
+    }
+}
