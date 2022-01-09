58f6fe59d310b691be7c60890bf69fb948d3b65b
Vstay <vstay@qq.com>
2022-01-09 22:32:24 +0800

    创建数组

---
+++ src/hello/array/Dome01Create.java
@@ -0,0 +1,8 @@
+package hello.array;
+
+public class Dome01Create {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.println(arr.length);
+    }
+}
