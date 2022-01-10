0581a7f4ef6e2ccf4c396631031c7dda4361de96
Vstay <vstay@qq.com>
2022-01-10 19:03:39 +0800

    创建对象时的内存分析

---
+++ src/hello/oop/demo02/memory/Application.java
@@ -0,0 +1,13 @@
+package hello.oop.demo02.memory;
+
+public class Application {
+    public static void main(String[] args) {
+        Pet dog = new Pet();
+        dog.name = "旺财";
+        dog.age = 3;
+        dog.shout();
+
+        System.out.println(dog.name);
+        System.out.println(dog.age);
+    }
+}
---
+++ src/hello/oop/demo02/memory/Pet.java
@@ -0,0 +1,9 @@
+package hello.oop.demo02.memory;
+
+public class Pet {
+    String name;
+    int age;
+    public void shout(){
+        System.out.println("汪汪汪");
+    }
+}
