5d7f5589a360c2df58cfc189f3be03ac612577a3
Vstay <vstay@qq.com>
2022-01-11 19:52:07 +0800

    重写

---
+++ src/hello/oop/demo06/rewrite/A.java
@@ -0,0 +1,13 @@
+package hello.oop.demo06.rewrite;
+
+public class A extends B{
+
+    /**
+     * '@xxx': 注解, 有功能的注释!
+     *  @Override: 重写.
+     */
+    @Override
+    public void test() {
+        System.out.println("A ==> test");
+    }
+}
