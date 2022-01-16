46c24b004d5f204d87be787a79587d7ed6e684d0
Vstay <vstay@qq.com>
2022-01-16 17:38:24 +0800

    删除有序数组中的重复项

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -6,14 +6,15 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        int[] nums = new int[]{2,7,11,15};
-        int target = 9;
-        for(int i =0;i<nums.length;i++){
-            for (int j=i+1;j<nums.length;j++)
-            {
-                if (nums[i]+nums[j] == target)
-                    System.out.println(""+i+""+j);
-            }
-        }
+        int[] nums =new int[]{1,1,2};
+        for(int i=0;i<nums.length;i++)
+            for(int j=i+1;j<nums.length;j++)
+                // nums[i] == nums[j]
+                if (nums[i] == nums[j])
+                {
+                    for(int k=j;k<nums.length;k++)
+                        nums[k-1] = nums[k];
+                }
+        System.out.println(nums);
     }
 }
