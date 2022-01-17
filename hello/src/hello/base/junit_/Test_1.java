48d2a35b4876af9189029d3f3be2779f18db77db
Vstay <vstay@qq.com>
2022-01-17 17:27:08 +0800

    Junit

---
+++ hello/src/hello/base/junit_/Test_1.java
@@ -0,0 +1,21 @@
+package hello.base.junit_;
+
+import org.junit.Test;
+
+public class Test_1 {
+    public static void main(String[] args) {
+
+    }
+
+
+    @Test
+    public void m1() {
+        System.out.println("m1被调用");
+
+    }
+
+    @Test
+    public void m2() {
+        System.out.println("m2被调用");
+    }
+}
