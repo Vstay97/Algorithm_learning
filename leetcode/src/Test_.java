e33b3ce7f980006cc02f85715ba847f63b49a72e
Vsaty <583429957@qq.com>
2022-03-21 20:12:52 +0800

    清理目录

--- leetcode/src/Test_.java
+++ leetcode/src/Test_.java
@@ -3,35 +3,15 @@ import java.util.Stack;
 public class Test_ {
     public static void main(String[] args) {
         Test_ test_ = new Test_();
-        String s = "()";
+        int m = 3;
+        int n = 3;
 
-        test_.isValid(s);
-
-    }
-    public boolean isValid(String s) {
-        Stack<Character> left = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{')
-                left.push(c);
-            else { // c 是右括号
-                // s 不为空, 并且左括号与右括号相匹配, 出栈
-                if (!left.empty() && charOf(c) == left.peek())
-                    left.pop();
-                else
-                    // 左括号与右括号不匹配
-                    return false;
-            }
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] += dp[1][i-1] + 1;
         }
-        // 判断是否全部出栈
-        return left.empty();
+
     }
 
-    public char charOf(char c) {
-        if (c == '(')
-            return ')';
-        if (c == '{')
-            return '}';
-        else
-            return ']';
-    }
+
 }
